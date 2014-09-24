e3c62ccd77a41be061e2961656072995afe427a7
Javier Carrillo <stringparser@gmail.com>
2014-09-24 10:05:20 +0200

    lint a little

--- index.js
+++ index.js
@@ -6,22 +6,18 @@ exports = module.exports = sites;
 
 function sites(frames, origin){
 
-  var originIs = type(origin), framesIs = type(frames);
   var limit = Error.stackTraceLimit;
   var prepare = Error.prepareStackTrace;
+  var originIs = type(origin), framesIs = type(frames);
 
   origin = framesIs.function || originIs.function || null;
   frames = framesIs.integer > 0 || framesIs.infinity ? frames : 1;
 
   Error.stackTraceLimit = origin ? frames : frames + 1;
-
-  Error.prepareStackTrace = function(err, stack){
-    return stack;
-  };
+  Error.prepareStackTrace = function(err, stack){ return stack; };
 
   var error = new Error();
   Error.captureStackTrace(error, origin || sites);
-
   var stack = origin ? error.stack : error.stack.slice(1);
 
   Error.stackTraceLimit = limit;
