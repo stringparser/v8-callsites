e909ea8c804d62b7927cd596f15f0e05a6f30a8c
Javier Carrillo <stringparser@gmail.com>
2014-09-25 16:38:33 +0200

    callsite method list

---
+++ methods.js
@@ -0,0 +1,18 @@
+module.exports = [
+  'getThis',
+  'getTypeName',
+  'isToplevel',
+  'isEval',
+  'getEvalOrigin',
+  'getScriptNameOrSourceURL',
+  'getFunction',
+  'getFunctionName',
+  'getMethodName',
+  'getFileName',
+  'getLineNumber',
+  'getColumnNumber',
+  'isNative',
+  'getPosition',
+  'isConstructor',
+  'toString'
+];
