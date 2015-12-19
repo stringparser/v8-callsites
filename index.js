d00c35a15c072832bdb83d6249e8a773e7a9bed3
Javier Carrillo <stringparser@gmail.com>
2015-12-19 15:04:22 +0100

    fix: bug fix for frames (to always be integer), remove utils-type dependency

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 'use strict';
 
-var type = require('utils-type');
-
 exports = module.exports = callsites;
 
 var limit = Error.stackTraceLimit;
 var trace = Error.prepareStackTrace;
 
 function callsites(frames, origin){
-  origin = type(origin || frames).function;
-  frames = Math.abs(type(frames).number) || 1;
+  origin = origin || frames;
+  frames = Math.abs(Math.floor(frames)) || 1;
+  origin = typeof origin === 'function' && origin;
 
   Error.stackTraceLimit = origin ? frames : frames + 1;
   Error.prepareStackTrace = function(_, stack){ return stack; };
